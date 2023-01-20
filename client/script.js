dd35d529bfccaacb016e93222969efb9df1d6b7f
ChrisspilDev <spiliotisch@me.com>
2023-01-20 23:01:55 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the server -> bot;s response
 
-  const response = await fetch('http://localhost:5000',  {
+  const response = await fetch('https://codeaichat-2o2r.onrender.com',  {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
